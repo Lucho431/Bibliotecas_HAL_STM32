a44427ddeba8c6b152fe4d072707d7e08d6e44b0
Lucho431 <luciano.salvatore431@gmail.com>
2023-01-02 16:49:26 -0300

    organizando bibliotecas

---
+++ bibliotecas_STM32/MPU9265/toCheck/tocheck_mpu9265.h
@@ -0,0 +1,31 @@
+// MPU9265.h
+
+#ifndef _MPU9265_h
+#define _MPU9265_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+struct Orientation
+{
+	float pitch;
+	float yaw;
+	float roll;
+	float height;
+	float pressure;
+	float temperature;
+};
+
+class MPU9265Class {
+ protected:
+
+ public:
+	void init();
+	void update();
+	Orientation orientation = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+};
+
+#endif
